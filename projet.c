b10c489c7103f09aedac53941db1499c74898214
Remadi Meriam <meriam.remadi@ensi-uma.tn>
2025-12-07 03:11:09 +0100

    Dernière version du projet

---
+++ projet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef enum {
+    STATUT_CONFIRMEE = 1,
+    STATUT_ANNULEE = 2,
+    STATUT_FACTUREE = 3
+} StatutReservation;
+
+int main() {
+    printf("Programme compilé avec succès.\n");
+    return 0;
+}
